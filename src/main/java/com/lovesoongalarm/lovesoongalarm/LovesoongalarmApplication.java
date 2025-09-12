c36e301a4e1d98b1ae60800af44399d86b0ccad8
SSUN <rlatjsghks4647@naver.com>
2025-09-12 17:02:41 +0900

    feat : LovesoongalarmApplication에 EnableScheduling 추가

--- src/main/java/com/lovesoongalarm/lovesoongalarm/LovesoongalarmApplication.java
+++ src/main/java/com/lovesoongalarm/lovesoongalarm/LovesoongalarmApplication.java
@@ -2,8 +2,10 @@ package com.lovesoongalarm.lovesoongalarm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LovesoongalarmApplication {
 
 	public static void main(String[] args) {
