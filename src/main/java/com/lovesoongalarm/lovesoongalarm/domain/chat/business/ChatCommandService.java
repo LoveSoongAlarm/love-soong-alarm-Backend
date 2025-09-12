97a1d3714fa5e52df290b858b68d1798c1493fe8
dmori <chosehoon2@naver.com>
2025-09-12 16:06:51 +0900

    docs: 로그 멘트 수정

--- src/main/java/com/lovesoongalarm/lovesoongalarm/domain/chat/business/ChatCommandService.java
+++ src/main/java/com/lovesoongalarm/lovesoongalarm/domain/chat/business/ChatCommandService.java
@@ -25,7 +25,7 @@ public class ChatCommandService {
         log.info("채팅방 생성 및 본인 참여 시작 - userId: {}, targetUserId: {}", userId, request.targetUserId());
         ChatRoom chatRoom = chatRoomService.createChatRoom(userId, request.targetUserId());
         chatRoomParticipantService.addParticipant(userId, request.targetUserId(), chatRoom);
-        log.info("채팅방 생성 및 본인 참여 시작 - userId: {}, chatRoomId: {}", userId, chatRoom.getId());
+        log.info("채팅방 생성 및 본인 참여 종료 - userId: {}, chatRoomId: {}", userId, chatRoom.getId());
         return chatRoomConverter.toCreateChatRoomResponse(chatRoom.getId());
     }
 }
