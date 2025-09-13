fa1d5e6ca32929456fbcbddd0ab5d3dfe2db4f3d
jeong724 <oliveyou724@gmail.com>
2025-09-13 16:08:45 +0900

    feat : 로그인 성공시 응답 핸들러 수정

--- src/main/java/com/lovesoongalarm/lovesoongalarm/security/handler/Oauth2SuccessHandler.java
+++ src/main/java/com/lovesoongalarm/lovesoongalarm/security/handler/Oauth2SuccessHandler.java
@@ -1,9 +1,11 @@
 package com.lovesoongalarm.lovesoongalarm.security.handler;
 
+import com.lovesoongalarm.lovesoongalarm.common.constant.Constants;
 import com.lovesoongalarm.lovesoongalarm.domain.user.persistence.entity.type.EUserStatus;
 import com.lovesoongalarm.lovesoongalarm.security.dto.JwtDTO;
 import com.lovesoongalarm.lovesoongalarm.security.info.AuthenticationResponse;
 import com.lovesoongalarm.lovesoongalarm.security.info.UserPrincipal;
+import com.lovesoongalarm.lovesoongalarm.utils.CookieUtil;
 import com.lovesoongalarm.lovesoongalarm.utils.JwtUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,13 +23,16 @@ import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
-public class Oauth2SuccessHandler implements AuthenticationSuccessHandler {
+public class Oauth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     @Value("${server.domain}")
     private String domain;
     private final JwtUtil jwtUtil;
     //private final JwtService jwtService;
     private final HttpSession session;
+
+    @Value("${jwt.redirect}")
+    private String REDIRECT_URI;
 
     @Override
     @Transactional
@@ -41,20 +47,26 @@ public class Oauth2SuccessHandler implements AuthenticationSuccessHandler {
         JwtDTO jwtDto = jwtUtil.generateTokens(principal.getUserId(), principal.getRole());
 
         //jwtService.updateRefreshToken(principal.getUserId(), jwtDto.refreshToken());
+
         boolean isRegistered = false;
         if (principal.getStatus() != null && principal.getStatus() == EUserStatus.ACTIVE) {
             isRegistered = true;
         }
 
-        AuthenticationResponse.makeLoginSuccessResponse(response, domain, jwtDto, jwtUtil.getRefreshExpiration(), isRegistered);
+        String accessToken = jwtDto.accessToken();
+        String refreshToken = jwtDto.refreshToken();
 
-//        String redirectUrl = (String) session.getAttribute("redirectUrl");
-//
-//        if (redirectUrl != null) {
-//            session.removeAttribute("redirectUrl");
-//            response.sendRedirect(redirectUrl);
-//        } else {
-//            response.sendRedirect("https://" + domain);
-//        }
+        // refreshToken은 쿠키에 심고
+        CookieUtil.addSecureCookie(
+                response,
+                domain,
+                Constants.REFRESH_COOKIE_NAME,
+                refreshToken,
+                jwtUtil.getRefreshExpiration()
+        );
+
+        String redirectUri = String.format(REDIRECT_URI, isRegistered, accessToken);
+
+        getRedirectStrategy().sendRedirect(request, response, redirectUri);
     }
 }
