132348d2a1937a74eeb9490bc9c4f085d44b849a
jeong724 <oliveyou724@gmail.com>
2025-09-14 08:45:21 +0900

    feat : 유저 프로필 수정 api 구현

--- src/main/java/com/lovesoongalarm/lovesoongalarm/domain/user/application/controller/UserController.java
+++ src/main/java/com/lovesoongalarm/lovesoongalarm/domain/user/application/controller/UserController.java
@@ -3,7 +3,9 @@ package com.lovesoongalarm.lovesoongalarm.domain.user.application.controller;
 import com.lovesoongalarm.lovesoongalarm.common.BaseResponse;
 import com.lovesoongalarm.lovesoongalarm.common.annotation.UserId;
 import com.lovesoongalarm.lovesoongalarm.domain.user.application.dto.OnBoardingRequestDTO;
+import com.lovesoongalarm.lovesoongalarm.domain.user.application.dto.UserInterestResponseDTO;
 import com.lovesoongalarm.lovesoongalarm.domain.user.application.dto.UserResponseDTO;
+import com.lovesoongalarm.lovesoongalarm.domain.user.application.dto.UserUpdateRequestDTO;
 import com.lovesoongalarm.lovesoongalarm.domain.user.business.UserQueryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -31,7 +33,7 @@ public class UserController {
         return BaseResponse.success(userQueryService.onBoardingUser(userId, request));
     }
 
-    @Operation(summary = "유저 상세 조회"
+    @Operation(summary = "유저 프로필 상세 조회"
             , description = "유저 상세 조회하는 API 입니다.")
     @ApiResponse(responseCode = "200", description = "유저 상세 조회 성공")
     @PatchMapping("/{user-id}")
@@ -41,5 +43,17 @@ public class UserController {
     ){
 
         return BaseResponse.success(userQueryService.getUser(targetId));
+    }
+
+    @Operation(summary = "유저 프로필 수정"
+            , description = "유저 프로필을 수정하는 API 입니다.")
+    @ApiResponse(responseCode = "200", description = "유저 프로필 수정 성공")
+    @PatchMapping("/me")
+    public BaseResponse<Void> updateUser(
+            @UserId Long userId,
+            @RequestBody UserUpdateRequestDTO updateRequest
+            ){
+
+        return BaseResponse.success(userQueryService.updateUser(userId, updateRequest));
     }
 }
