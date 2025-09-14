d66b2c7fb2d6550123391b56492fb023fbf63ebf
jeong724 <oliveyou724@gmail.com>
2025-09-15 02:00:03 +0900

    fix : 관심사 수정

--- src/main/java/com/lovesoongalarm/lovesoongalarm/domain/user/sub/interest/persistence/type/EDetailLabel.java
+++ src/main/java/com/lovesoongalarm/lovesoongalarm/domain/user/sub/interest/persistence/type/EDetailLabel.java
@@ -27,37 +27,36 @@ public enum EDetailLabel {
     RUNNING("러닝"),
     FITNESS("헬스"),
     CLIMBING("클라이밍"),
-    BOXING("복싱"),
+    MMA("격투기"),
     SWIMMING("수영"),
     BOARD("보드"),
-    GYMNASTICS("체조"),
-    DANCE("댄스"),
+    ETC_SPORTS("기타 운동"),
 
     // 스포츠 (Sports)
     KBO("KBO"),
     KLEAGUE("K리그"),
     OVERSEAS_SOCCER("해외축구"),
     ESPORTS("e스포츠"),
-    BASKETBALL("농구"),
-    VOLLEYBALL("배구"),
-    MOTORSPORTS("모터스포츠"),
+    KBL("KBL"),
+    V_LEAGUE("V-리그"),
+    F1("F1"),
 
     // 독서 (Reading)
     NOVEL("소설"),
-    ESSAY("에세이"),
     POETRY("시집"),
     WEBNOVEL("웹소설"),
-    SELF_DEVELOPMENT("자기계발서"),
+    ETC_READING("기타"),
 
     // 패션 (Fashion)
     STREET("스트릿"),
     VINTAGE("빈티지"),
     FASHION_CLASSIC("클래식"),
-    COLLECTING("수집"),
+    SUBCULTURE("서브컬쳐"),
 
-    // 식도락 (Food)
+    // 라이프스타일 (LIFESTYLE)
     RESTAURANT("맛집탐방"),
-    CAFE("카페"),
+    PICTURE("사진"),
+    DIY("DIY"),
     COOKING("요리"),
 
     // 여행 (Travel)
