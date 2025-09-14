46299576befdf250b21d50b634e1933e82cbfe0d
dmori <chosehoon2@naver.com>
2025-09-14 20:04:32 +0900

    feat: 일대일 채팅이므로 메시지별 읽음 상태 관리 테이블 제거

--- src/main/java/com/lovesoongalarm/lovesoongalarm/domain/chat/sub/room/sub/message/persistence/repository/MessageReadStatusRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lovesoongalarm.lovesoongalarm.domain.chat.sub.room.sub.message.persistence.repository;
-
-import com.lovesoongalarm.lovesoongalarm.domain.chat.sub.room.sub.message.persistence.entity.MessageReadStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageReadStatusRepository extends JpaRepository<MessageReadStatus, Long> {
-}
