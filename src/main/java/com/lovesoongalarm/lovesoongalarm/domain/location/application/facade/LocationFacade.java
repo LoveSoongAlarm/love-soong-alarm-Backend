6568a5a277fb0fa3cad9e0290e3ece8823d073ef
SSUN <rlatjsghks4647@naver.com>
2025-09-16 02:29:44 +0900

    feat : LocationFacade 구현

---
+++ src/main/java/com/lovesoongalarm/lovesoongalarm/domain/location/application/facade/LocationFacade.java
@@ -0,0 +1,75 @@
+package com.lovesoongalarm.lovesoongalarm.domain.location.application.facade;
+
+import com.lovesoongalarm.lovesoongalarm.domain.location.application.dto.MatchingResultDTO;
+import com.lovesoongalarm.lovesoongalarm.domain.location.application.dto.NearbyResponseDTO;
+import com.lovesoongalarm.lovesoongalarm.domain.location.application.dto.NearbyUserResponseDTO;
+import com.lovesoongalarm.lovesoongalarm.domain.location.business.LocationService;
+import com.lovesoongalarm.lovesoongalarm.domain.location.implement.RedisPipeline;
+import com.lovesoongalarm.lovesoongalarm.domain.user.application.dto.UserResponseDTO;
+import com.lovesoongalarm.lovesoongalarm.domain.user.business.UserQueryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class LocationFacade {
+    private final LocationService locationService;
+    private final UserQueryService userQueryService;
+    private final RedisPipeline redisPipeline;
+
+    @Transactional
+    public void updateLocation(Long userId, double latitude, double longitude) {
+        locationService.updateLocation(userId, latitude, longitude);
+    }
+
+    @Transactional
+    public NearbyResponseDTO findNearby(Long userId) {
+        try {
+            List<NearbyUserResponseDTO> nearbyUserResponse = new ArrayList<>();
+            MatchingResultDTO matchingResult = locationService.findNearby(userId);
+
+            log.info("matchingResult: {}", matchingResult);
+
+            List<Object> pipeLastSeenResults = redisPipeline.pipe(ops -> {
+                for (Long id : matchingResult.userIds()) {
+                    ops.opsForValue().get("lastseen:" + id);
+                }
+            });
+
+            int i = 0;
+            for (Long id : matchingResult.userIds()) {
+                UserResponseDTO user = userQueryService.getUser(id);
+                log.info("userResponseDTO: {}", user);
+
+                String lastSeenStr = (String) pipeLastSeenResults.get(i++);
+                long lastSeen = (lastSeenStr != null) ? Long.parseLong(lastSeenStr) : 0L;
+                log.info("{} lastSeen: {}", id, lastSeen);
+
+                nearbyUserResponse.add(NearbyUserResponseDTO.builder()
+                        .name(user.name())
+                        .age(user.age())
+                        .major(user.major())
+                        .lastSeen(lastSeen)
+                        .emoji(user.emoji())
+                        .matchCount(matchingResult.userMatchCounts().getOrDefault(id, 0L))
+                        .build());
+            }
+
+            log.info("nearbyUserResponse: {}", nearbyUserResponse);
+
+            return NearbyResponseDTO.builder()
+                    .matchCount(matchingResult.matchCount())
+                    .nearbyUsersInformation(nearbyUserResponse)
+                    .build();
+        } catch (Exception e) {
+            log.error("findNearby() failed. userId={}", userId, e);
+            throw e;
+        }
+    }
+}
