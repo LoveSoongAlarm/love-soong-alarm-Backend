c411293e2e30d6e000897e8ec63be5250fc1c9ab
SSUN <rlatjsghks4647@naver.com>
2025-09-15 11:02:17 +0900

    feat : LocationController에서 userId param 제거

--- src/main/java/com/lovesoongalarm/lovesoongalarm/domain/location/application/controller/LocationController.java
+++ src/main/java/com/lovesoongalarm/lovesoongalarm/domain/location/application/controller/LocationController.java
@@ -1,6 +1,7 @@
 package com.lovesoongalarm.lovesoongalarm.domain.location.application.controller;
 
 import com.lovesoongalarm.lovesoongalarm.common.BaseResponse;
+import com.lovesoongalarm.lovesoongalarm.common.annotation.UserId;
 import com.lovesoongalarm.lovesoongalarm.domain.location.application.dto.LocationRequest;
 import com.lovesoongalarm.lovesoongalarm.domain.location.business.LocationService;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +17,7 @@ public class LocationController {
 
     @PostMapping("/update")
     public BaseResponse<Void> update(
-            @RequestParam Long userId,
+            @UserId Long userId,
             @RequestBody LocationRequest locationRequest
     ) {
         locationService.updateLocation(userId, locationRequest.latitude(), locationRequest.longitude());
@@ -25,7 +26,7 @@ public class LocationController {
 
     @GetMapping("/nearby")
     public BaseResponse<List<Long>> nearby(
-            @RequestParam Long userId
+            @UserId Long userId
     ) {
         return BaseResponse.success(locationService.findNearby(userId));
     }
