93969b7497339c60f2f293eb249d7a0fec274fdd
SSUN <rlatjsghks4647@naver.com>
2025-09-16 02:30:51 +0900

    feat : LocationController Facade 주입으로 변경

--- src/main/java/com/lovesoongalarm/lovesoongalarm/domain/location/application/controller/LocationController.java
+++ src/main/java/com/lovesoongalarm/lovesoongalarm/domain/location/application/controller/LocationController.java
@@ -3,31 +3,30 @@ package com.lovesoongalarm.lovesoongalarm.domain.location.application.controller
 import com.lovesoongalarm.lovesoongalarm.common.BaseResponse;
 import com.lovesoongalarm.lovesoongalarm.common.annotation.UserId;
 import com.lovesoongalarm.lovesoongalarm.domain.location.application.dto.LocationRequest;
-import com.lovesoongalarm.lovesoongalarm.domain.location.business.LocationService;
+import com.lovesoongalarm.lovesoongalarm.domain.location.application.dto.NearbyResponseDTO;
+import com.lovesoongalarm.lovesoongalarm.domain.location.application.facade.LocationFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/location")
 @RequiredArgsConstructor
 public class LocationController {
-    private final LocationService locationService;
+    private final LocationFacade locationFacade;
 
     @PostMapping("/update")
     public BaseResponse<Void> update(
             @UserId Long userId,
             @RequestBody LocationRequest locationRequest
     ) {
-        locationService.updateLocation(userId, locationRequest.latitude(), locationRequest.longitude());
+        locationFacade.updateLocation(userId, locationRequest.latitude(), locationRequest.longitude());
         return BaseResponse.success(null);
     }
 
     @GetMapping("/nearby")
-    public BaseResponse<List<Long>> nearby(
+    public BaseResponse<NearbyResponseDTO> nearby(
             @UserId Long userId
     ) {
-        return BaseResponse.success(locationService.findNearby(userId));
+        return BaseResponse.success(locationFacade.findNearby(userId));
     }
 }
