b6d4537ba3d1c05b05f20daee4eed87177f029b9
SSUN <96679633+SeonHwan-Kim@users.noreply.github.com>
2025-09-16 00:25:40 +0900

    feat : location update 수정
    * fix : api 경로 수정
    
    * fix : NearbyRequest -> LocationRequest로 수정
    
    * feat : LocationController에서 userId param 제거
    
    * feat : 사용자 취향 정보 Redis에 저장
    
    * feat : redis executePipelined 분리

--- src/main/java/com/lovesoongalarm/lovesoongalarm/domain/location/application/controller/LocationController.java
+++ src/main/java/com/lovesoongalarm/lovesoongalarm/domain/location/application/controller/LocationController.java
@@ -1,7 +1,8 @@
 package com.lovesoongalarm.lovesoongalarm.domain.location.application.controller;
 
 import com.lovesoongalarm.lovesoongalarm.common.BaseResponse;
-import com.lovesoongalarm.lovesoongalarm.domain.location.application.dto.NearbyRequest;
+import com.lovesoongalarm.lovesoongalarm.common.annotation.UserId;
+import com.lovesoongalarm.lovesoongalarm.domain.location.application.dto.LocationRequest;
 import com.lovesoongalarm.lovesoongalarm.domain.location.business.LocationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -9,23 +10,23 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/location")
+@RequestMapping("/api/location")
 @RequiredArgsConstructor
 public class LocationController {
     private final LocationService locationService;
 
     @PostMapping("/update")
     public BaseResponse<Void> update(
-            @RequestParam Long userId,
-            @RequestBody NearbyRequest nearbyRequest
+            @UserId Long userId,
+            @RequestBody LocationRequest locationRequest
     ) {
-        locationService.updateLocation(userId, nearbyRequest.latitude(), nearbyRequest.longitude());
+        locationService.updateLocation(userId, locationRequest.latitude(), locationRequest.longitude());
         return BaseResponse.success(null);
     }
 
     @GetMapping("/nearby")
     public BaseResponse<List<Long>> nearby(
-            @RequestParam Long userId
+            @UserId Long userId
     ) {
         return BaseResponse.success(locationService.findNearby(userId));
     }
