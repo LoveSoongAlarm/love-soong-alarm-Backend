8236a22e7f38adc1392ddb51eec4e9a5971c01ac
dmori <chosehoon2@naver.com>
2025-09-16 03:40:10 +0900

    feat: 메시지 엔티티 및 기본 인프라 설정

--- src/main/java/com/lovesoongalarm/lovesoongalarm/domain/chat/sub/room/sub/message/persistence/entity/Message.java
+++ src/main/java/com/lovesoongalarm/lovesoongalarm/domain/chat/sub/room/sub/message/persistence/entity/Message.java
@@ -4,9 +4,11 @@ import com.lovesoongalarm.lovesoongalarm.domain.chat.sub.room.persistence.entity
 import com.lovesoongalarm.lovesoongalarm.domain.user.persistence.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -14,16 +16,18 @@ import java.time.LocalDateTime;
 @Getter
 @Table(name="messages")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Message {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
 
     @CreatedDate
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -33,4 +37,19 @@ public class Message {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    @Builder
+    private Message(String content, ChatRoom chatRoom, User user) {
+        this.content = content;
+        this.chatRoom = chatRoom;
+        this.user = user;
+    }
+
+    public static Message create(String content, ChatRoom chatRoom, User user) {
+        return Message.builder()
+                .content(content)
+                .chatRoom(chatRoom)
+                .user(user)
+                .build();
+    }
 }
