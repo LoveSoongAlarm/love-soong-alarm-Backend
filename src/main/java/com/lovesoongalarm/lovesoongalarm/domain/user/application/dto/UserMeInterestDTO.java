4b576f3d42294f2e8b27869c784dbd0c5d268b99
차현정 <138744022+jeong724@users.noreply.github.com>
2025-09-17 08:20:07 +0900

    Feat #73 자신의 프로필 조회 API 구현 (#74)
    * feat : 유저 컨트롤러 추가
    
    * feat : dto 추가
    
    * feat : 서비스 로직 추가
    
    * feat : dto 수정
    
    * feat : dto 수정

--- src/main/java/com/lovesoongalarm/lovesoongalarm/domain/user/application/dto/UserMeInterestDTO.java
+++ src/main/java/com/lovesoongalarm/lovesoongalarm/domain/user/application/dto/UserMeInterestDTO.java
@@ -1,22 +1,23 @@
 package com.lovesoongalarm.lovesoongalarm.domain.user.application.dto;
 
-import com.lovesoongalarm.lovesoongalarm.domain.user.persistence.entity.User;
 import com.lovesoongalarm.lovesoongalarm.domain.user.sub.interest.persistence.entity.Interest;
 import lombok.Builder;
 
 import java.util.List;
 
 @Builder
-public record UserInterestResponseDTO(
+public record UserMeInterestDTO(
+        String label,
         String detailLabel,
         List<String> hashTags
 ) {
-    public static UserInterestResponseDTO from(Interest interest){
-        return UserInterestResponseDTO.builder()
+    public static UserMeInterestDTO from(Interest interest){
+        return UserMeInterestDTO.builder()
                 .detailLabel(interest.getDetailLabel().getValue())
+                .label(interest.getLabel().getValue())
                 .hashTags(interest.getHashtags().stream().map(
-                        hashtag -> hashtag.getLabel()
-                    ).toList()
+                                hashtag -> hashtag.getLabel()
+                        ).toList()
                 )
                 .build();
     }
