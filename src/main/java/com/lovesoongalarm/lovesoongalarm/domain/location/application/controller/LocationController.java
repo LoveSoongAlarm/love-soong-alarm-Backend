ca3c74772a05e4d104d1624cc5f487fb11917298
SSUN <rlatjsghks4647@naver.com>
2025-09-16 18:55:07 +0900

    feat : LocationController swagger 추가

--- src/main/java/com/lovesoongalarm/lovesoongalarm/domain/location/application/controller/LocationController.java
+++ src/main/java/com/lovesoongalarm/lovesoongalarm/domain/location/application/controller/LocationController.java
@@ -5,15 +5,22 @@ import com.lovesoongalarm.lovesoongalarm.common.annotation.UserId;
 import com.lovesoongalarm.lovesoongalarm.domain.location.application.dto.LocationRequest;
 import com.lovesoongalarm.lovesoongalarm.domain.location.application.dto.NearbyResponseDTO;
 import com.lovesoongalarm.lovesoongalarm.domain.location.application.facade.LocationFacade;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/location")
 @RequiredArgsConstructor
+@Tag(name = "Location", description = "위치정보 API")
 public class LocationController {
     private final LocationFacade locationFacade;
 
+    @Operation(summary = "위치 업데이트"
+            , description = "위치 정보를 업데이트하는 API 입니다.")
+    @ApiResponse(responseCode = "200", description = "위치 업데이트 성공")
     @PostMapping("/update")
     public BaseResponse<Void> update(
             @UserId Long userId,
@@ -23,6 +30,9 @@ public class LocationController {
         return BaseResponse.success(null);
     }
 
+    @Operation(summary = "주변사람 검색"
+            , description = "주변 50m 안에 있는 사람을 검색하는 API 입니다.")
+    @ApiResponse(responseCode = "200", description = "주변사람 검색 성공")
     @GetMapping("/nearby")
     public BaseResponse<NearbyResponseDTO> nearby(
             @UserId Long userId
