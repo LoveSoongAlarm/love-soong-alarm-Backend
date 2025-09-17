51c60f872ee259b44f104ab36c5f577d939fb709
SSUN <rlatjsghks4647@naver.com>
2025-09-17 21:08:38 +0900

    feat : Notice convertere 추가

---
+++ src/main/java/com/lovesoongalarm/lovesoongalarm/domain/notice/application/converter/NoticeConverter.java
@@ -0,0 +1,18 @@
+package com.lovesoongalarm.lovesoongalarm.domain.notice.application.converter;
+
+import com.lovesoongalarm.lovesoongalarm.domain.notice.application.dto.NoticeResponseDTO;
+import com.lovesoongalarm.lovesoongalarm.domain.notice.persistence.entity.Notice;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NoticeConverter {
+    public NoticeResponseDTO toNoticeResponseDTO(Notice notice) {
+        return NoticeResponseDTO.builder()
+                .userId(notice.getUser().getId())
+                .matchingUserId(notice.getMatchingUserId())
+                .noticeTime(notice.getNoticeTime())
+                .message(notice.getMessage())
+                .status(notice.getStatus().getValue())
+                .build();
+    }
+}
