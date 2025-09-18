69acafb556dd4f2f2b41c845994d35fb8b3a0e75
dmori <chosehoon2@naver.com>
2025-09-18 15:24:46 +0900

    fix: 잘못 import된 @Param 해결

--- src/main/java/com/lovesoongalarm/lovesoongalarm/domain/user/persistence/repository/UserRepository.java
+++ src/main/java/com/lovesoongalarm/lovesoongalarm/domain/user/persistence/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package com.lovesoongalarm.lovesoongalarm.domain.user.persistence.repository;
 
 import com.lovesoongalarm.lovesoongalarm.domain.user.persistence.entity.User;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
