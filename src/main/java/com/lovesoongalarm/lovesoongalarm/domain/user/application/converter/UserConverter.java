307a0b44ad628786a2d47e0c307efca376cc9dc0
Dmori <83327857+Dimo-2562@users.noreply.github.com>
2025-09-20 23:09:26 +0900

    feat: 유저 티켓 개수 조회 API 구현 (#139)

--- src/main/java/com/lovesoongalarm/lovesoongalarm/domain/user/application/converter/UserConverter.java
+++ src/main/java/com/lovesoongalarm/lovesoongalarm/domain/user/application/converter/UserConverter.java
@@ -1,6 +1,7 @@
 package com.lovesoongalarm.lovesoongalarm.domain.user.application.converter;
 
 import com.lovesoongalarm.lovesoongalarm.domain.user.application.dto.UserSlotResponseDTO;
+import com.lovesoongalarm.lovesoongalarm.domain.user.application.dto.UserTicketResponseDTO;
 import com.lovesoongalarm.lovesoongalarm.domain.user.persistence.entity.User;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +10,12 @@ public class UserConverter {
     public UserSlotResponseDTO createSlotInfo(User user) {
         return UserSlotResponseDTO.builder()
                 .maxSlots(user.getMaxSlot())
+                .build();
+    }
+
+    public UserTicketResponseDTO createTicketInfo(User user) {
+        return UserTicketResponseDTO.builder()
+                .chatTicket(user.getChatTicket())
                 .build();
     }
 }
