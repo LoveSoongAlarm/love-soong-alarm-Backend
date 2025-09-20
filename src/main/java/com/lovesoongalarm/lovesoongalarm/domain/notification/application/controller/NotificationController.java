10e748b2b932740a5d2dfee300b4fc9cf94592b0
SSUN <96679633+SeonHwan-Kim@users.noreply.github.com>
2025-09-20 15:39:01 +0900

    feat : 알림 swagger 추가 (#121)

--- src/main/java/com/lovesoongalarm/lovesoongalarm/domain/notification/application/controller/NotificationController.java
+++ src/main/java/com/lovesoongalarm/lovesoongalarm/domain/notification/application/controller/NotificationController.java
@@ -4,6 +4,9 @@ import com.lovesoongalarm.lovesoongalarm.common.BaseResponse;
 import com.lovesoongalarm.lovesoongalarm.common.annotation.UserId;
 import com.lovesoongalarm.lovesoongalarm.domain.notification.application.dto.NotificationResponseDTO;
 import com.lovesoongalarm.lovesoongalarm.domain.notification.business.NotificationQueryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -13,9 +16,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/notifications")
 @RequiredArgsConstructor
+@Tag(name = "Notification", description = "매칭 알림 API")
 public class NotificationController {
     private final NotificationQueryService notificationQueryService;
 
+    @Operation(summary = "알림 정보"
+            , description = "알림 정보를 불러오는 API 입니다.")
+    @ApiResponse(responseCode = "200", description = "알림 불러오기 성공")
     @GetMapping("")
     public BaseResponse<List<NotificationResponseDTO>> notice(
             @UserId Long userId
@@ -23,6 +30,9 @@ public class NotificationController {
         return BaseResponse.success(notificationQueryService.notification(userId));
     }
 
+    @Operation(summary = "개별 알림 상태 업데이트"
+            , description = "개별 알림 상태를 읽음으로 업데이트하는 API 입니다.")
+    @ApiResponse(responseCode = "200", description = "알림 읽음처리 성공")
     @PatchMapping("/read/{notificationId}")
     public BaseResponse<Void> readNotification(
             @UserId Long userId,
@@ -32,6 +42,9 @@ public class NotificationController {
         return BaseResponse.success(null);
     }
 
+    @Operation(summary = "전체 알림 상태 업데이트"
+            , description = "전체 알림 상태를 읽음으로 업데이트하는 API 입니다.")
+    @ApiResponse(responseCode = "200", description = "전체 알림 읽음처리 성공")
     @PatchMapping("read-all")
     public BaseResponse<Void> readAllNotifications(
             @UserId Long userId
@@ -40,6 +53,9 @@ public class NotificationController {
         return BaseResponse.success(null);
     }
 
+    @Operation(summary = "개별 알림 삭제"
+            , description = "개별 알림을 삭제하는 API 입니다.")
+    @ApiResponse(responseCode = "200", description = "알림 삭제 성공")
     @DeleteMapping("/{notificationId}")
     public BaseResponse<Void> deleteNotification(
             @UserId Long userId,
@@ -49,6 +65,9 @@ public class NotificationController {
         return BaseResponse.success(null);
     }
 
+    @Operation(summary = "전체 알림 삭제"
+            , description = "전체 알림을 삭제하는 API 입니다.")
+    @ApiResponse(responseCode = "200", description = "전체 알림 삭제 성공")
     @DeleteMapping("")
     public BaseResponse<Void> deleteAllNotifications(
             @UserId Long userId
