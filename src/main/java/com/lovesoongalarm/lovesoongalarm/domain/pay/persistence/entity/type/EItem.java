ac5eafa16e186e056c7b58f37015a915c633d80d
jeong724 <oliveyou724@gmail.com>
2025-09-21 23:53:35 +0900

    feat : 가격 수정

--- src/main/java/com/lovesoongalarm/lovesoongalarm/domain/pay/persistence/entity/type/EItem.java
+++ src/main/java/com/lovesoongalarm/lovesoongalarm/domain/pay/persistence/entity/type/EItem.java
@@ -8,9 +8,9 @@ import lombok.RequiredArgsConstructor;
 public enum EItem {
     PREPASS(3900, "prepass"),
     CHAT_TICKET(1500, "chat_ticket"),
-    SLOT_1(500, "slot_1"),
-    SLOT_2(700, "slot_2"),
-    SLOT_3(1000, "slot_3");
+    SLOT_1(1000, "slot_1"),
+    SLOT_2(1200, "slot_2"),
+    SLOT_3(1300, "slot_3");
 
     private final int price;
     private final String key;
