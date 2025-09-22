0d0aff61edf209418c4715771a33e5dbe1a32cdd
dmori <chosehoon2@naver.com>
2025-09-22 13:20:35 +0900

    feat: 초기 채팅방 조회 시 내가 차단됐는지 여부 삭제

--- src/main/java/com/lovesoongalarm/lovesoongalarm/domain/chat/sub/room/application/dto/BlockStatus.java
+++
@@ -1,7 +0,0 @@
-package com.lovesoongalarm.lovesoongalarm.domain.chat.sub.room.application.dto;
-
-public record BlockStatus(
-        boolean isPartnerBlocked,
-        boolean isBlockedByPartner
-) {
-}
