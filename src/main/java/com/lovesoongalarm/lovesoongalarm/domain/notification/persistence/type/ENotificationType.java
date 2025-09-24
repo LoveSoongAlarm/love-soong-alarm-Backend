3b441a74fb33230f5dbe65bd50b9d42a6a474076
dmori <chosehoon2@naver.com>
2025-09-24 19:28:55 +0900

    feat: DTO 구현

--- src/main/java/com/lovesoongalarm/lovesoongalarm/domain/notification/persistence/type/ENotificationType.java
+++ src/main/java/com/lovesoongalarm/lovesoongalarm/domain/notification/persistence/type/ENotificationType.java
@@ -1,6 +1,18 @@
 package com.lovesoongalarm.lovesoongalarm.domain.notification.persistence.type;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ENotificationType {
-    READ,
-    DELETE
+    READ("메시지 읽음"),
+    DELETE("메시지 삭제"),
+    CHAT_MESSAGE("채팅 메시지");
+
+    private final String description;
+
+    public String getDescription() {
+        return description;
+    }
 }
