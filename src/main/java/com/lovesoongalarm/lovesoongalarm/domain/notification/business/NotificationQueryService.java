fd4098f33ca145603abf6150ceb60a9bd7e08439
SSUN <rlatjsghks4647@naver.com>
2025-09-24 20:37:21 +0900

    Conflict : 충돌 해결

--- src/main/java/com/lovesoongalarm/lovesoongalarm/domain/notification/business/NotificationQueryService.java
+++ src/main/java/com/lovesoongalarm/lovesoongalarm/domain/notification/business/NotificationQueryService.java
@@ -23,6 +23,7 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.UnexpectedRollbackException;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
